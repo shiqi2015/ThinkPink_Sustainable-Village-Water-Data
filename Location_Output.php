8960bcd0eab49212456ff8140987e814399978fa
songmaharrison <31391441+songmaharrison@users.noreply.github.com>
2017-10-22 08:55:38 -0400

    Update Location_Output.php

--- Location_Output.php
+++ Location_Output.php
@@ -26,28 +26,40 @@
 <?php
 while (!$Location_Output->eof()) {
 	session_start();
-	$inputLongtitude=$_POST["Longtitude"];
-	$_SESSION['long'] = $input;
-	$inputLatitude=$_POST["Latitude"];
-	$inputL_Name=$_POST["L_Name"];
-    $inputState=$_POST["Province/State"];
+	if (isset($_POST['Longtitude'])) 
+	{  
+    $inputLongtitude=$_POST["Longtitude"];
+    }
+	$_SESSION['long'] = $inputLongtitude;
+	if (isset($_POST['Latitude'])) 
+	{  
+    $inputLatitude=$_POST["Latitude"];
+    }
+    if (isset($_POST['Location_Name'])) 
+	{  
+	$inputL_Name=$_POST["Location_Name"];
+    }
+    if (isset($_POST['State'])) 
+	{  
+    $inputState=$_POST["State"];
+    }
 
     $localhost = "localhost";
 	$user = "root";
 	$password = "";
-	$database = "Location";
+	$database = "GLOBAL_WATER";
 	
 	$connect = mysqli_connect($localhost,$user,$password,$database);
 
-	$queryLongtitude = "SELECT * FROM Location WHERE Longtitude = '$inputLongtitude'";
-	$queryLatitude = "SELECT * FROM Location WHERE Email = '$inputLatitude'";
-	$queryL_Name = "SELECT * FROM Location WHERE Email = '$inputL_Name'";
-	$queryState = "SELECT * FROM Location WHERE Email = '$inputState'";
+	$queryLongtitude = "SELECT * FROM LOCATION WHERE Longtitude = '$inputLongtitude'";
+	$queryLatitude = "SELECT * FROM LOCATION WHERE Latitude = '$inputLatitude'";
+	$queryL_Name = "SELECT * FROM LOCATION WHERE L_Name = '$inputL_Name'";
+	$queryState = "SELECT * FROM LOCATION WHERE State = '$inputState'";
 
-	$resultLongtitude= mysqli::query($queryLongtitude);
-	$resultLatitude = mysqli::query($queryLatitude);
-	$resultL_Name = mysqli::query($queryL_Name);
-	$resultState = mysqli::query($queryState);
+	$resultLongtitude= $link->query($queryLongtitude);
+	$resultLatitude = $link->query($queryLatitude);
+	$resultL_Name = $link->query($queryL_Name);
+	$resultState = $link->query($queryState);
 
 	echo "<tr>";
 	echo "<td>" . $resultL_Name . "</td>";
